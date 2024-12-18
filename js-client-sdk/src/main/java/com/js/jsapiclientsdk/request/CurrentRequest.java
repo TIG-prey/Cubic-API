843f9dd50dd1176c36dd67bce32d7e46a9c5105d
sakisaki <189444236@qq.com>
2024-12-18 10:44:57 +0800

    封装业务的请求信息

---
+++ js-client-sdk/src/main/java/com/js/jsapiclientsdk/request/CurrentRequest.java
@@ -0,0 +1,50 @@
+package com.js.jsapiclientsdk.request;
+
+import com.js.jsapiclientsdk.response.ResultResponse;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+/**
+ * 封装业务的请求信息
+ * @author sakisaki
+ * @date 2024/12/17 23:37
+ */
+@Accessors(chain = true)
+@Setter
+public class CurrentRequest extends BaseRequest<Object, ResultResponse> {
+    /**
+     * 请求类型
+     */
+    private String method;
+    /**
+     * 请求路径
+     */
+    private String path;
+
+    /**
+     * 重写BaseRequest中获取请求类型的方法
+     * @return String
+     */
+    @Override
+    public String getMethod() {
+        return method;
+    }
+
+    /**
+     * 重写BaseRequest中获取请求路径的方法
+     * @return String
+     */
+    @Override
+    public String getPath() {
+        return path;
+    }
+
+    /**
+     * 重写BaseRequest中获取响应类方法
+     * @return Class<ResultResponse>
+     */
+    @Override
+    public Class<ResultResponse> getResponseClass() {
+        return ResultResponse.class;
+    }
+}
