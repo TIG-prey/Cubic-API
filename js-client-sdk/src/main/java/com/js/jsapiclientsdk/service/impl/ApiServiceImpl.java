4c06c4c8401ede5cbf31d03323ac34e9161e92af
sakisaki <189444236@qq.com>
2024-12-23 23:26:03 +0800

    定义 ApiServiceImpl 实现类

---
+++ js-client-sdk/src/main/java/com/js/jsapiclientsdk/service/impl/ApiServiceImpl.java
@@ -0,0 +1,13 @@
+package com.js.jsapiclientsdk.service.impl;
+
+import com.js.jsapiclientsdk.service.ApiService;
+import com.js.jsapiclientsdk.service.BaseService;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ *
+ * @author sakisaki
+ * @date 2024/12/23 17:45
+ */
+@Slf4j
+public class ApiServiceImpl extends BaseService implements ApiService {}
