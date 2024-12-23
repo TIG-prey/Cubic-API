1471a04a8c5a2f0d661db269c85529bacf84efe1
sakisaki <189444236@qq.com>
2024-12-23 23:26:48 +0800

    重构JsApi客户端配置

--- js-client-sdk/src/main/java/com/js/jsapiclientsdk/config/JsApiClientConfig.java
+++ js-client-sdk/src/main/java/com/js/jsapiclientsdk/config/JsApiClientConfig.java
@@ -1,7 +1,11 @@
 package com.js.jsapiclientsdk.config;
 
 import com.js.jsapiclientsdk.client.JsApiClient;
+import com.js.jsapiclientsdk.service.ApiService;
+import com.js.jsapiclientsdk.service.BaseService;
+import com.js.jsapiclientsdk.service.impl.ApiServiceImpl;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -27,10 +31,23 @@ public class JsApiClientConfig {
      * 秘密密钥
      */
     private String secretKey;
+    /**
+     * 网关
+     */
+    private String host;
 
     @Bean
     public JsApiClient jsApiClient() {
         return new JsApiClient(accessKey, secretKey);
     }
 
+    @Bean
+    public ApiService apiService() {
+        ApiServiceImpl apiServiceImpl = new ApiServiceImpl();
+        apiServiceImpl.setJsApiClient(new JsApiClient(accessKey, secretKey));
+        if (StringUtils.isNotBlank(host)) {
+            apiServiceImpl.setGatewayHost(host);
+        }
+        return apiServiceImpl;
+    }
 }
